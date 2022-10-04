1e60d1b061741362f67f489e0872afd6f1499f8e
Caio Felipe <101940168+Caio-Fe@users.noreply.github.com>
2022-10-04 20:05:50 -0300

    Update criarUsuarios.php

--- 3dawAV1Caio/criarUsuarios.php
+++ 3dawAV1Caio/criarUsuarios.php
@@ -11,30 +11,27 @@ $senha = "";
 $bancodeDados = "daw20222manha";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $arquivo = $_POST["filename"];
     
     $conn = new mysqli($servidor, $usuario, $senha, $bancodeDados);
     if ($conn->connect_error) {
         die("Conexao com o banco de dados falhou!!!");
     }
-
-    $usuariosArquivo = fopen("$arquivo.txt", "r") or die("Erro na abertura do arquivo");
-    while (!feof($usuariosArquivo)) {
-        $linhas[] = fgets($usuariosArquivo);
+        $coluna = explode(";", file_get_contents($_FILES['filename']['tmp_name']));
+        for($x = 0;$x<sizeof($coluna);$x+=5){
+            $y=$x+1; 
+            $z=$x+2; 
+            $w=$x+3; 
+            $a=$x+4;
+            $sql = "Insert into usuario (`nome`, `email`, `senha`,`tipo`,`perfil`) VALUES ('$coluna[$x]','$coluna[$y]','$coluna[$z]','$coluna[$w]','$coluna[$a]')";
+            $conn->query($sql);
+        }
     }
-    fclose($usuariosArquivo);
-    foreach ($linhas as $linha) {
-            $coluna = explode(";", $linha);
-            //$txt = "$nome;$email;$senha;$tipo;$perfil\n";
-            $sql = "Insert into usuario (`nome`, `email`, `senha`,`tipo`,`perfil`) VALUES ('$coluna[0]','$coluna[1]','$coluna[2]','$coluna[3]','$coluna[4]')";
 
-    }
-        $result = $conn->query($sql);
-} else {
-    $ehPost = false;
-}
 ?>
 <h1>Criar Usuarios</h1>
+<pre>Documento de texto: dados separados por ponto-e-virgula, ultimo dado do arquivo sem ponto-e-virgula.
+        exemplo: nome1;email1;senha1;tipo1;perfil1;
+                 nome2;email1;senha1;tipo1;perfil2</pre>
 <br>
 <a href="homeAV1.html">Pagina Inicial</a><br>
 <a href="criarDisciplina.php">Criar Disciplina</a><br> <!-- formulario-->
@@ -45,9 +42,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <a href="criarUsuarios.php">Criar Usuarios</a><br> <!-- a partir de arquivo, inserir no banco o conteúdo do arquivo-->
 <a href="listarTodosUsuarios.php">Listar todos os Usuarios</a><br>
 <br>
-<form action="criarUsuarios.php" method=POST>
-  <input type="file" id="myFile" name="filename">
-  <input type="submit" value="Enviar Usuarios do Arquivo">
+<form enctype="multipart/form-data" action="criarUsuarios.php" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" /> 
+    <input type="file" name="filename">
+    <input type="submit" value="Enviar Usuarios do Arquivo">
 </form>
 
 <br>
